cda2bbd4fa8a1ab2a5a15c6fc35f1e03e2db7ce5
BlainedZA <blainedza@gmail.com>
2020-03-30 10:50:49 +0200

    BallControl Added

---
+++ Pong/Assets/BallControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+
+    private RigidBody2D rb2d;
+    public ScriptIWantToAccess GMScript;
+
+
+    public void GoBall()
+    {
+        float random = Random.range(0, 2);
+        if (random < 1)
+        {
+            rb2d.AddForce(new Vector2(20, -15));
+        } else
+        {
+            rb2d.AddForce(new Vector2(-20, -15));
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<RigidBody2D>();
+        invoke("GoBall", 2);
+    }
+
+    void ResetBall()
+    {
+        GM.DestroyBall();
+        GM.SpawnBall();
+
+    }
+
+    void RestartGame()
+    {
+        ResetBall();
+        Invoke("GoBall", 1);
+    }
+
+}
--- Pong/Assets/GameSetup.cs
+++ Pong/Assets/GameSetup.cs
@@ -7,33 +7,31 @@ public class GameSetup : MonoBehaviour {
 	public GameObject ballPrefab;
 	public GameObject clone;
 
-	void Awake ()
+	public void SpawnBall ()
     {
 		clone = Instantiate(ballPrefab, new Vector2(0, 0), Quaternion.identity) as GameObject;
-		
 	}
-
 
 
 	// Update is called once per frame
 	void Update () {
-		Vector2 BallPos = clone.transform.position;
-		if (BallPos.y < -5.5)
-		{
-			if (clone)
-            {
-				return;
-            }
-			else
-            {
+		
+		if(clone)
+        {
+			Vector2 BallPos = clone.transform.position;
+			if (BallPos.y < -5.5)
+			{
 				DestroyBall();
 			}
-			
 		}
+		else
+        {
+			return;
+        }
 	}
 
-	void DestroyBall ()
+	public void DestroyBall ()
     {
-		UnityEngine.Object.Destroy(clone);
+		Object.Destroy(clone);
 	}
 }
